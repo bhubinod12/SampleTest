7097bc6a13206d3561cd5dbeaff9d03c6e0df8d8
Binod Kumar Yadav <binod.yadav@motorolasolutions.com>
2022-07-17 20:11:09 +0530

    few more testing

--- test.js
+++ test.js
@@ -1,1 +1,33 @@
 console.log('This is for test purpose !!!');
+
+// Swap two number without third variable
+
+// let a = 5;
+// let b = 10;
+
+// a =  a+b;
+// b = a-b;
+// a = a-b;
+// console.log('The value of a is ' + a);
+// console.log('The value of a is ' + b);
+
+// Find leap year.
+
+let year = 2100;
+
+if (year % 4 === 0) {
+    if (year % 100 === 0) {
+        if (year % 400 === 0) {
+            console.log('The year is a leap year');
+        } else {
+            console.log('The year is not a leap year');
+        }
+
+    } else {
+        console.log('The year is a leap year');
+    }
+} else {
+    console.log('The year is not a leap year');
+}
+
+
